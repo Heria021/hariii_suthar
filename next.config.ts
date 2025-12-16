46e49354985a24742ced1ee5c8a08980f60708c5
Heria021 <hariomsuthar7143@gmail.com>
2025-12-16 21:18:12 +0530

    github.com/hariii_suthar/hariii_suthar

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,11 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  output: 'export',
+  basePath: '/hariii_suthar', 
+  images: {
+    unoptimized: true,
+  },
 };
 
 export default nextConfig;
