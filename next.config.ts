78e2bfbe7c8638caddc55deeafc992288fef143e
Heria021 <hariomsuthar7143@gmail.com>
2025-12-16 21:24:41 +0530

    remove config changes

--- next.config.ts
+++ next.config.ts
@@ -1,11 +1,7 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  output: 'export',
-  basePath: '/hariii_suthar', 
-  images: {
-    unoptimized: true,
-  },
+  /* config options here */
 };
 
 export default nextConfig;
